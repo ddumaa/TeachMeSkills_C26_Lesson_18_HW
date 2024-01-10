0e1f1a1b3a5a9867aca7ae3e2edcd04479f552f7
ddumaa <dimaduma62@gmail.com>
2024-01-11 01:12:20 +0300

    complete

--- src/homework/task1/Runner.java
+++ src/homework/task1/Runner.java
@@ -1,4 +1,21 @@
 package homework.task1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Runner {
+    public static void main(String[] args) {
+        List<Integer> randomInt = new ArrayList<>();
+        for(int i = 0; i <=20; i++) {
+            randomInt.add(i, (int)(Math.random() * 10));
+        }
+        System.out.println(randomInt);
+        System.out.println("Removing duplicates " + randomInt.stream().distinct().toList());
+        System.out.println("Even numbers from 7th to 17th position " + randomInt.stream().skip(6).limit(11).filter(n -> n%2 ==0).toList());
+        System.out.println("Multiplying each element " + randomInt.stream().map(n -> n * 2).toList());
+        System.out.println("Sorting and outputting the first 4 elements " + randomInt.stream().sorted().limit(4).toList());
+        System.out.println("Number of elements in stream " + randomInt.stream().count());
+        System.out.println("Average value " + randomInt.stream().collect(Collectors.averagingInt(i -> i)));
+    }
 }
--- src/homework/task2/Runner.java
+++ src/homework/task2/Runner.java
@@ -1,4 +1,27 @@
 package homework.task2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Runner {
+    public static void main(String[] args) {
+        List<String> name = new ArrayList<>();
+        name.add("Dmitriy");
+        name.add("Polina");
+        name.add("Mark");
+        name.add("Daniil");
+        name.add("Liisa");
+        name.add("Stepan");
+        name.add("Artur");
+        name.add("Ivan");
+        name.add("Andrey");
+        name.add("nameTG: Strfend");
+        name.add("Sergey");
+
+        List<String> collect = name.stream().sorted().findFirst().stream().collect(Collectors.toList());
+        System.out.println(collect);
+        List<String> collect2 = name.stream().filter(n -> n.toLowerCase().charAt(0) == 'a').collect(Collectors.toList());
+        System.out.println(collect2);
+    }
 }
